1b5c113442f5b5789325dd1fa378a59620823cb3
opscury <viska.valdis@gmail.com>
2019-06-14 17:18:15 +0300

    Test Room

---
+++ Escape Room Project/Assets/HandReparentRight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandReparentRight : MonoBehaviour
+{
+    // Resets the parent after load so handtracking works properly
+    void Start()
+    {
+        Invoke("SetParent", 1);
+    }
+
+    private void SetParent()
+    {
+        transform.SetParent(GameObject.Find("hand_right").transform);
+    }
+}
