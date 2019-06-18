07febcf735f4da7626d53e43dc9fa74f99bfbc8d
Sinantrarion <sinantrarion@gmail.com>
2019-06-18 11:35:58 +0300

    I hate my life
    I hate my life

---
+++ Escape Room Project/Assets/Scripts/IHateMyLife.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IHateMyLife : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I hate my life");
+        //I hate my life
+
+        /*
+         * I hate my life 
+         */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
