5a7af1b43d7e64d7baa4af9cb80a045fecce84dd
Sinantrarion <sinantrarion@gmail.com>
2019-06-21 20:26:53 +0300

    Revert "Merge branch 'master' of https://github.com/Opscury/VR-Grupa"
    Quickly made character-cube(movement wasd + qe to turn), left mouse button to summon the projectile, that will fly and bounces from walls. projectiledeletes itself after 5 second because why not. You can change it's lifetime and speed through script added to it.

---
+++ Escape Room Project/Assets/Scripts/CharContr.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharContr : MonoBehaviour
+{
+    public float movementSpeed;
+    public GameObject LaserPrefab;
+
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
+        }
+        else if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+    
+
+        if (Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, -5, 0, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.E) && !Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, 5, 0, Space.Self);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject Laser = Instantiate(LaserPrefab, transform.position, transform.rotation);
+        }
+    }
+}
---
+++ Escape Room Project/Assets/Scripts/LaserScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    public int speed = 500;
+    public int lifeTime = 5;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    IEnumerator DeathTimer()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+     
+    void Start()
+    {
+        StartCoroutine(DeathTimer());
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.AddForce(transform.forward * speed);
+    }
+
+   /*private void Update()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 newPosition = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.LookAt(newPosition);
+        transform.Translate(newPosition, Space.World);
+    } */
+}
